8ffa45a63c7a79bf2c40b58e2abb0d27caa8a8c2
anggraeniastri <astrianggraeni22@yahoo.co.id>
2017-06-01 09:13:09 +0700

    Add files via upload

---
+++ merk.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ASTRI
+ */
+public class merk implements baranglama{
+
+    @Override
+    public void kondisi() {
+        System.out.println("kondisi baik"); 
+    }
+
+    @Override
+    public void diskon() {
+        System.out.println("Diskon 20%"); 
+    }
+    
+}
